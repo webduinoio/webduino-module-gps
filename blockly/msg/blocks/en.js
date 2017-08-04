a0dd4ae519567edae3b29c9ca37c3c277a09c07e
osk2 <sc60714@gmail.com>
2017-08-04 14:34:53 +0800

    Update translation

--- blockly/msg/blocks/en.js
+++ blockly/msg/blocks/en.js
@@ -1,15 +1,15 @@
 // GPS
-Blockly.Msg.WEBDUINO_GPS_RX = "GPS (TinyGPS)，RX ";
+Blockly.Msg.WEBDUINO_GPS_RX = "GPS (TinyGPS), RX ";
 Blockly.Msg.WEBDUINO_GPS_TX = "  TX";
 Blockly.Msg.WEBDUINO_GPS_LOCATE = "Location:";
 Blockly.Msg.WEBDUINO_GPS_TIME = "Time:";
-Blockly.Msg.WEBDUINO_GPS_GET_NOW = "read Info:";
-Blockly.Msg.WEBDUINO_GPS_GET_CORD = "Get coordinate with time";
+Blockly.Msg.WEBDUINO_GPS_GET_NOW = "Read Info:";
+Blockly.Msg.WEBDUINO_GPS_GET_CORD = "Get Coordinate with Time";
 Blockly.Msg.WEBDUINO_GPS_EVERY = "run every";
-Blockly.Msg.WEBDUINO_GPS_RUN_EVERY_MS = "millisecond";
+Blockly.Msg.WEBDUINO_GPS_RUN_EVERY_MS = "Millisecond";
 Blockly.Msg.WEBDUINO_GPS_DO = "Do";
-Blockly.Msg.WEBDUINO_GPS_CURRENT_VALUE = "Current value of";
-Blockly.Msg.WEBDUINO_GPS_CURRENT_LATITUDE = "latitude";
-Blockly.Msg.WEBDUINO_GPS_CURRENT_LONGITUDE = "longtitude";
-Blockly.Msg.WEBDUINO_GPS_CURRENT_DATE = "date";
-Blockly.Msg.WEBDUINO_GPS_CURRENT_TIME = "time";
+Blockly.Msg.WEBDUINO_GPS_CURRENT_VALUE = "Current Value of";
+Blockly.Msg.WEBDUINO_GPS_CURRENT_LATITUDE = "Latitude";
+Blockly.Msg.WEBDUINO_GPS_CURRENT_LONGITUDE = "Longtitude";
+Blockly.Msg.WEBDUINO_GPS_CURRENT_DATE = "Date";
+Blockly.Msg.WEBDUINO_GPS_CURRENT_TIME = "Time";
